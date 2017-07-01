83fc0244a1edaeb1b2e2375fe32c70eac8ba6341
SoengSaravit <soengsaravit@gmail.com>
2017-07-01 09:01:23 +0700

    Initial Init

--- UISliderDemo/ViewController.swift
+++ UISliderDemo/ViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var textLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        
+        textLabel.text = "\(slider.value)"
+        
     }
-
+    
 
 }
 
